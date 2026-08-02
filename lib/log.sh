c72d054656de74c5a99e1d20e29531f66d718cfa
qj0r9j0vc2 <me@qj0r9j0vc2.me>
2026-08-02 14:01:43 +0900

    feat(node-recorder): query and classify the ALERTS metric

--- lib/log.sh
+++ lib/log.sh
@@ -1,7 +1,7 @@
 # shellcheck shell=bash
 
 log_info() {
-  printf '%s [INFO] %s\n' "$(date -u +%Y-%m-%dT%H:%M:%SZ)" "$*"
+  printf '%s [INFO] %s\n' "$(date -u +%Y-%m-%dT%H:%M:%SZ)" "$*" >&2
 }
 
 log_error() {
